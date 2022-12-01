38e2c0b920c41b59d6fd4791c431238b5251e3e6
Kai Jellinghaus <contact@kaij.tech>
2022-12-01 07:47:22 +0100

    Day1 Rust solution

--- rust/src/day1.rs
+++ rust/src/day1.rs
@@ -4,6 +4,56 @@
  * you can either work with chars or bytes (accept &str or &[u8])
  * or use a generator w/ custom type via #[aoc_generator(dayx, partx)]
  * you can name implementations via #[aoc(dayx, partx, NAME)]
- * 
+ *
  * That's it. Have fun!
  */
+
+use std::collections::BinaryHeap;
+
+pub struct Elf {
+    calories: Vec<u64>,
+}
+
+#[aoc_generator(day1, part1)]
+#[aoc_generator(day1, part2)]
+pub fn generator(input: &str) -> Vec<Elf> {
+    let mut vec = Vec::new();
+    vec.push(Elf {
+        calories: Vec::new(),
+    });
+    input.lines().fold(vec, |mut acc, e| {
+        let acc_len = acc.len();
+        assert!(acc_len >= 1);
+        if e.len() == 0 {
+            acc.push(Elf {
+                calories: Vec::new(),
+            });
+        } else {
+            acc.get_mut(acc_len - 1)
+                .expect("acc.len > 0")
+                .calories
+                .push(e.parse().unwrap());
+        }
+        acc
+    })
+}
+
+#[aoc(day1, part1)]
+pub fn solve_part1(input: &[Elf]) -> u64 {
+    input
+        .into_iter()
+        .map(|e| e.calories.iter().sum())
+        .max()
+        .unwrap()
+}
+
+#[aoc(day1, part2)]
+pub fn solve_part2(input: &[Elf]) -> u64 {
+    input
+        .into_iter()
+        .map(|e| e.calories.iter().sum::<u64>())
+        .collect::<BinaryHeap<_>>()
+        .into_iter_sorted()
+        .take(3)
+        .sum()
+}
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,9 +1,10 @@
+#![feature(binary_heap_into_iter_sorted)]
+
 extern crate aoc_runner;
 
 #[macro_use]
 extern crate aoc_runner_derive;
-extern crate crypto;
 
 pub mod day1;
 
-aoc_lib!{ year = 2022 }
+aoc_lib! { year = 2022 }
